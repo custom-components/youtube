211e8318466ddaf702cd31c60402c36ee90e141b
LiJu09 <lisojuraj@gmail.com>
2020-12-11 10:50:35 +0100

    Implemented API to sensor.py

--- custom_components/youtube/sensor.py
+++ custom_components/youtube/sensor.py
@@ -6,6 +6,7 @@ https://github.com/custom-components/youtube
 """
 
 import html
+import json
 import logging
 import async_timeout
 import voluptuous as vol
@@ -15,13 +16,15 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
 
 CONF_CHANNEL_ID = 'channel_id'
+CONF_API_KEY = 'api_key'
 
 ICON = 'mdi:youtube'
 
-BASE_URL = 'https://www.youtube.com/feeds/videos.xml?channel_id={}'
+BASE_URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=date&channelId={}&key={}'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_CHANNEL_ID): cv.string,
+    vol.Required(CONF_API_KEY): cv.string,
 })
 
 _LOGGER = logging.getLogger(__name__)
@@ -31,30 +34,32 @@ async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None):  # pylint: disable=unused-argument
     """Setup sensor platform."""
     channel_id = config['channel_id']
+    api_key = config['api_key']
     session = async_create_clientsession(hass)
     try:
-        url = BASE_URL.format(channel_id)
+        url = BASE_URL.format(channel_id, api_key)
         async with async_timeout.timeout(10, loop=hass.loop):
             response = await session.get(url)
             info = await response.text()
-        name = html.parser.HTMLParser().unescape(
-            info.split('<title>')[1].split('</')[0])
+        data = json.loads(info)
+        name = data['items'][0]['snippet']['channelTitle']
     except Exception:  # pylint: disable=broad-except
 
         name = None
 
     if name is not None:
-        async_add_entities([YoutubeSensor(channel_id, name, session)], True)
+        async_add_entities([YoutubeSensor(channel_id, api_key, name, session)], True)
 
 class YoutubeSensor(Entity):
     """YouTube Sensor class"""
-    def __init__(self, channel_id, name, session):
+    def __init__(self, channel_id, api_key, name, session):
         self._state = None
         self.session = session
         self._image = None
         self.live = False
         self._name = name
         self.channel_id = channel_id
+        self.api_key = api_key
         self.url = None
         self.published = None
 
@@ -62,21 +67,21 @@ class YoutubeSensor(Entity):
         """Update sensor."""
         _LOGGER.debug('%s - Running update', self._name)
         try:
-            url = BASE_URL.format(self.channel_id)
+            url = BASE_URL.format(self.channel_id, self.api_key)
             async with async_timeout.timeout(10, loop=self.hass.loop):
                 response = await self.session.get(url)
                 info = await response.text()
-            title = html.parser.HTMLParser().unescape(
-                info.split('<title>')[2].split('</')[0])
-            url = info.split('<link rel="alternate" href="')[2].split('"/>')[0]
-            if self.live or url != self.url:
-                self.live = await is_live(url, self._name, self.hass, self.session)
+            data = json.loads(info)
+            title = data['items'][0]['snippet']['title']
+            url = 'https://www.youtube.com/watch?v=' + data['items'][0]['id']['videoId']
+            live_info = data['items'][0]['snippet']['liveBroadcastContent']
+            if live_info == 'live':
+                self.live = True
             else:
                 _LOGGER.debug('%s - Skipping live check', self._name)
             self.url = url
-            self.published = info.split('<published>')[2].split('</')[0]
-            thumbnail_url = info.split(
-                '<media:thumbnail url="')[1].split('"')[0]
+            self.published = data['items'][0]['snippet']['publishTime']
+            thumbnail_url = data['items'][0]['snippet']['thumbnails']['high']['url']
             self._state = title
             self._image = thumbnail_url
         except Exception as error:  # pylint: disable=broad-except
@@ -108,18 +113,3 @@ class YoutubeSensor(Entity):
         return {'url': self.url,
                 'published': self.published,
                 'live': self.live}
-
-
-async def is_live(url, name, hass, session):
-    """Return bool if channel is live"""
-    returnvalue = False
-    try:
-        async with async_timeout.timeout(10, loop=hass.loop):
-            response = await session.get(url)
-            info = await response.text()
-        if 'Started streaming' in info:
-            returnvalue = True
-            _LOGGER.debug('%s - Latest Video is live', name)
-    except Exception as error:  # pylint: disable=broad-except
-        _LOGGER.debug('%s - Could not update - %s', name, error)
-    return returnvalue

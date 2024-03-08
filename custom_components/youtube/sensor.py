4d7e3cc4104ef782110acfd439fa877b15407beb
Jonny Bergdahl <bergdahl@users.noreply.github.com>
2024-03-08 09:08:43 +0100

    Add new Consent cookie Add debug logging Code cleanup

--- custom_components/youtube/sensor.py
+++ custom_components/youtube/sensor.py
@@ -17,11 +17,10 @@ import re
 import html
 
 CONF_CHANNEL_ID = 'channel_id'
-
 ICON = 'mdi:youtube'
-
 BASE_URL = 'https://www.youtube.com/feeds/videos.xml?channel_id={}'
 CHANNEL_LIVE_URL = 'https://www.youtube.com/channel/{}'
+COOKIES = {"SOCS": "CAESEwgDEgk0ODE3Nzk3MjQaAmVuIAEaBgiA_LyaBg"}
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_CHANNEL_ID): cv.string,
@@ -34,19 +33,22 @@ async def async_setup_platform(
         hass, config, async_add_entities, discovery_info=None):  # pylint: disable=unused-argument
     """Setup sensor platform."""
     channel_id = config['channel_id']
+    _LOGGER.debug(f'Setting up {channel_id}')
     session = async_create_clientsession(hass)
     try:
         url = BASE_URL.format(channel_id)
         async with async_timeout.timeout(10):
             response = await session.get(url)
             info = await response.text()
-        name = info.split('<title>')[1].split('</')[0]
+            name = info.split('<title>')[1].split('</')[0]
     except Exception as error:  # pylint: disable=broad-except
-        _LOGGER.debug('Unable to set up - %s', error)
+        _LOGGER.error(f'Unable to set up {channel_id} - {error}')
         name = None
 
     if name is not None:
-        async_add_entities([YoutubeSensor(channel_id, name, session)], True)
+        sensor = YoutubeSensor(channel_id, name, session)
+        async_add_entities([sensor], True)
+
 
 class YoutubeSensor(Entity):
     """YouTube Sensor class"""
@@ -70,7 +72,7 @@ class YoutubeSensor(Entity):
 
     async def async_update(self):
         """Update sensor."""
-        _LOGGER.debug('%s - Running update', self._name)
+        _LOGGER.debug(f'{self._name} - Running update')
         try:
             url = BASE_URL.format(self.channel_id)
             async with async_timeout.timeout(10):
@@ -83,9 +85,9 @@ class YoutubeSensor(Entity):
             title = info.split('<title>')[2].split('</')[0]
             url = info.split('<link rel="alternate" href="')[2].split('"/>')[0]
             if self.live or url != self.url:
-                self.stream, self.live, self.stream_start = await is_live(url, self._name, self.hass, self.session)
+                self.stream, self.live, self.stream_start = await self.is_live(url)
             else:
-                _LOGGER.debug('%s - Skipping live check', self._name)
+                _LOGGER.debug(f'{self._name} - Skipping live check')
             self.url = url
             self.content_id = url.split('?v=')[1]
             self.published = info.split('<published>')[2].split('</')[0]
@@ -95,10 +97,10 @@ class YoutubeSensor(Entity):
             self._image = thumbnail_url
             self.stars = info.split('<media:starRating count="')[1].split('"')[0]
             self.views = info.split('<media:statistics views="')[1].split('"')[0]
-            url = CHANNEL_LIVE_URL.format(self.channel_id)
-            self.channel_live, self.channel_image = await is_channel_live(url, self.name, self.hass, self.session)
+
+            self.channel_live, self.channel_image = await self.is_channel_live()
         except Exception as error:  # pylint: disable=broad-except
-            _LOGGER.debug('%s - Could not update - %s', self._name, error)
+            _LOGGER.debug(f'{self._name} - Could not update - {error}')
 
     @property
     def name(self):
@@ -139,37 +141,43 @@ class YoutubeSensor(Entity):
                 'channel_is_live': self.channel_live,
                 'channel_image': self.channel_image}
 
-async def is_live(url, name, hass, session):
-    """Return bool if video is stream and bool if video is live"""
-    live = False
-    stream = False
-    start = None
-    try:
-        async with async_timeout.timeout(10):
-            response = await session.get(url, cookies=dict(CONSENT="YES+cb"))
-            info = await response.text()
-        if 'isLiveBroadcast' in info:
-            stream = True
-            start = parse(info.split('startDate" content="')[1].split('"')[0])
-            if 'endDate' not in info:
-                live = True
-                _LOGGER.debug('%s - Latest Video is live', name)
-    except Exception as error:  # pylint: disable=broad-except
-        _LOGGER.debug('%s - Could not update - %s', name, error)
-    return stream, live, start
+    async def is_live(self, url):
+        """Return bool if video is stream and bool if video is live"""
+        live = False
+        stream = False
+        start = None
+        try:
+            async with async_timeout.timeout(10):
+                response = await self.session.get(url)
+                html = await response.text()
+            if 'isLiveBroadcast' in html:
+                stream = True
+                start = parse(html.split('startDate" content="')[1].split('"')[0])
+                if 'endDate' not in html:
+                    live = True
+                    _LOGGER.debug(f'{self._name} - Latest Video is live')
+        except Exception as error:  # pylint: disable=broad-except
+            _LOGGER.debug(f'{self._name} - is_live(): Error {error}')
+        return stream, live, start
 
-async def is_channel_live(url, name, hass, session):
-    """Return bool if channel is live"""
-    live = False
-    try:
-        async with async_timeout.timeout(10):
-            response = await session.get(url, cookies=dict(CONSENT="YES+cb"))
-            info = await response.text()
-        if '{"iconType":"LIVE"}' in info:
-            live = True
-            _LOGGER.debug('%s - Channel is live', name)
-        regex = r"\"width\":48,\"height\":48},{\"url\":\"(.*?)\",\"width\":88,\"height\":88},{\"url\":"
-        channel_image = re.findall(regex, info, re.MULTILINE)[0].replace("=s88-c-k-c0x00ffffff-no-rj", "")
-    except Exception as error:  # pylint: disable=broad-except
-        _LOGGER.debug('%s - Could not update - %s', name, error)
-    return live, channel_image
+    async def is_channel_live(self):
+        """Return bool if channel is live"""
+        live = False
+        channel_image = None
+        url = CHANNEL_LIVE_URL.format(self.channel_id)
+        try:
+            _LOGGER.debug("GET %s: %s", self._name, url)
+            async with async_timeout.timeout(10):
+                response = await self.session.get(url, cookies = COOKIES)
+                html = await response.text()
+            if '{"iconType":"LIVE"}' in html:
+                live = True
+                _LOGGER.debug(f'{self._name} - Channel is live')
+            regex = r"\"width\":48,\"height\":48},{\"url\":\"(.*?)\",\"width\":88,\"height\":88},{\"url\":"
+            found = re.findall(regex, html, re.MULTILINE)
+            if found:
+                channel_image = found[0]
+                channel_image = channel_image.replace("=s88-c-k-c0x00ffffff-no-rj", "")
+        except Exception as error:  # pylint: disable=broad-except
+            _LOGGER.debug(f'{self._name} - is_channel_live(): Error {error}')
+        return live, channel_image
